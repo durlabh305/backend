9bb82a8c35dde06f37f408981f079933128c6579
alokmeetupsystems <alok@meetupsystems.com>
2024-04-25 20:26:05 +0530

    added mvc

---
+++ Models/User.js
@@ -0,0 +1,22 @@
+// models/User.js
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  bookmarks: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Story' }]
+});
+
+userSchema.methods.hashPassword = async function() {
+  this.password = await bcrypt.hash(this.password, 10);
+};
+
+userSchema.methods.comparePassword = async function(candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
