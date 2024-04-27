fda6f8a774834ee1fca47ee6474105db43388546
alokmeetupsystems <alok@meetupsystems.com>
2024-04-27 19:34:44 +0530

    added likes

--- Controllers/StoryController.js
+++ Controllers/StoryController.js
@@ -1,10 +1,20 @@
 const storyModels = require('../Models/Story');
+const slugify = require('slugify');
 
 async function createStory(req, res) {
     try {
         let data = req.body;
-        let created_story = storyModels.create(data);
-         return res.status(400).send({ status: "success", data: created_story });
+        let created_story = await storyModels.create(data);
+         // Generate a slug from the story title
+         const slug = slugify(data.title, { lower: true, remove: /[*+~.()'"!:@]/g });
+
+         // Construct the link using story ID and slug
+         const link = `/stories/${created_story._id}/${slug}`;
+ 
+         // Update the created story with the generated link
+         const updatedStory = await storyModels.findByIdAndUpdate(created_story._id, { link }, { new: true });
+ 
+         return res.status(400).send({ status: "success", data: created_story , link });
     } catch (error) {
         console.log(`error ${error}`);
          return res.status(4500).send({ status: "Failed", msg: error.message });
@@ -28,6 +38,18 @@ async function getAllStory(req, res) {
     try {
         const story = await storyModels.find();
         return res.status(200).json({ status: "success", data: story })
+    } catch (error) {
+        console.log(error)
+       return res.status(500).json({ status: "Failed", message: 'Something went wrong' })
+    }
+}
+
+async function getAllStoryBYId(req, res) {
+    try {
+        let storyId = req.params.id;
+        let story = await storyModels.findById(storyId);
+        let storyLink = story.link
+        return res.status(200).json({ status: "success", data: storyLink })
     } catch (error) {
         console.log(error)
        return res.status(500).json({ status: "Failed", message: 'Something went wrong' })
--- Models/Story.js
+++ Models/Story.js
@@ -6,7 +6,9 @@ const storySchema = new mongoose.Schema({
   content: { type: String, required: true },
   category: { type: String, enum: ['food', 'health_fitness', 'travel', 'movie', 'education'], required: true },
   // author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  likes: { type: Number, default: 0 }
+  likes: { type: Number, default: 0 },
+  link:{type :String, default:"kkk"}
+
 });
 
 const Story = mongoose.model('Story', storySchema);
